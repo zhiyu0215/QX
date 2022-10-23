7a2e81c0423a8d83dd2ce17996b27c03569c3f27
zhiyu <30713899+zhiyu0215@users.noreply.github.com>
2022-10-23 15:48:41 +0800

    Update and rename Script/Crack_APP/notability.js to Script/Unlock_APP/notability.js

--- Script/Unlock_APP/notability.js
+++ Script/Unlock_APP/notability.js
@@ -6,6 +6,6 @@
 #第四步：解锁成功以后，记得设置里面打开iCloud同步
 #第五步：同步以后，永久解锁，无论卸载安装，支持最新版本，以及老版本
 
-^https?:\/\/notability\.com\/subscriptions url script-response-body https://raw.githubusercontent.com/zhiyu0215/QX/main/Script/Crack_APP/notability.js
+^https?:\/\/notability\.com\/subscriptions url script-response-body https://raw.githubusercontent.com/zhiyu0215/QX/main/Script/Unlock_APP/notability.js
 
 hostname = notability.com
