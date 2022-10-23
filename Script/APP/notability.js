24d1b3c03681636d6e17f4929ed2c65959ad5115
zhiyu <30713899+zhiyu0215@users.noreply.github.com>
2022-10-23 15:08:14 +0800

    Create notability.js

---
+++ Script/APP/notability.js
@@ -0,0 +1,11 @@
+#Notability +解锁订阅
+#软件说明：
+#第一步：自己下载老版本—版本11.7.1或者11.7.2版本…等等
+#第二步：下载圈X解锁脚本
+#第三步：规则下载以后，先开规则，在进入软件点击恢复即可
+#第四步：解锁成功以后，记得设置里面打开iCloud同步
+#第五步：同步以后，永久解锁，无论卸载安装，支持最新版本，以及老版本
+
+^https?:\/\/notability\.com\/subscriptions url script-response-body 
+
+hostname = notability.com
